6408b14f314e13723f6d0685724d7a9c7fa4ba92
Nikolay Alexeev <kodi_kodi_kodi@mail.ru>
2020-10-28 23:25:48 +0500

    create auth modal

--- js/main.js
+++ js/main.js
@@ -7,4 +7,55 @@ const toggleModal = () => modal.classList.toggle('is-open')
 cartButton.addEventListener('click', toggleModal)
 close.addEventListener('click', toggleModal)
 
-new WOW().init()
+const buttonAuth = document.querySelector('.button-auth')
+const modalAuth = document.querySelector('.modal-auth')
+const closeAuth = document.querySelector('.close-auth')
+const logInForm = document.querySelector('#logInForm')
+const logInInput = document.querySelector('#login')
+const userName = document.querySelector('.user-name')
+const buttonOut = document.querySelector('.button-out')
+
+let login = localStorage.getItem('deliveryFoodLogin')
+
+const toggleModalAuth = () => modalAuth.classList.toggle('is-open')
+
+const authorized = () => {
+
+    const logOut = () => {
+        login = null
+        localStorage.removeItem('deliveryFoodLogin')
+        buttonAuth.style.display = ''
+        userName.style.display = ''
+        buttonOut.style.display = ''
+        buttonOut.removeEventListener('click', logOut)
+        checkAuth()
+    }
+
+    userName.textContent = login
+    buttonAuth.style.display = 'none'
+    userName.style.display = 'inline'
+    buttonOut.style.display = 'block'
+    buttonOut.addEventListener('click', logOut)
+}
+
+const notAuthorized = () => {
+
+    const logIn = (event) => {
+        event.preventDefault()
+        login = logInInput.value
+        localStorage.setItem('deliveryFoodLogin', login)
+        toggleModalAuth()
+        buttonAuth.removeEventListener('click', toggleModalAuth)
+        closeAuth.removeEventListener('click', toggleModalAuth)
+        logInForm.removeEventListener('submit', logIn)
+        logInForm.reset()
+        checkAuth()
+    }
+
+    buttonAuth.addEventListener('click', toggleModalAuth)
+    closeAuth.addEventListener('click', toggleModalAuth)
+    logInForm.addEventListener('submit', logIn)
+}
+
+const checkAuth = () => login ? authorized() : notAuthorized()
+checkAuth()
